b10e0eee261679b628ca82b1008a65709e1a1d0c
Derek <nightwing917@hotmail.com>
2020-05-13 19:21:50 -0600

    added set video progress on event listener

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatPlayIcon);
 video.addEventListener('play', updatPlayIcon);
 video.addEventListener('timeupdate', updateProgress);
+
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
